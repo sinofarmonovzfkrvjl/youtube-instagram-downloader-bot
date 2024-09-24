b901b09fe65ff12b0d375e61ea5d31506d914821
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2024-09-24 07:53:43 -0700

    update

--- downloader.py
+++ downloader.py
@@ -2,11 +2,14 @@ import yt_dlp
 import requests
 
 def YouTubeVideoDownloader(url, save_path='.'):
-    yt_opts = {
-        'outtmpl': f'{save_path}/video.mp4'          # yt-dlp best version is 2024.08.01, 2024.08.06
-    }
-    with yt_dlp.YoutubeDL(yt_opts) as yt:
-        yt.download([url])
+    try:
+        yt_opts = {
+            'outtmpl': f'{save_path}/video.mp4'          # yt-dlp best version is 2024.08.01, 2024.08.06
+        }
+        with yt_dlp.YoutubeDL(yt_opts) as yt:
+            yt.download([url])
+    except:
+        return False
     return yt.extract_info(url, download=False)
 
 def InstagramDownloader(url):
